9b4bbdb4075840384c52e621f8a759e5655bddb6
naba0224 <nblikeit@gmail.com>
2026-09-14 18:40:44 +0800

    some fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import "./globals.css";
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
 import {AuthProvider} from "@/context/AuthContext";
 import {Toaster} from "react-hot-toast";
-
-const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "File Server Admin",
@@ -14,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="font-sans">
         <AuthProvider>{children}</AuthProvider>
         <Toaster
           position="top-right"
